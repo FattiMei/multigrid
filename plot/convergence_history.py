fc97bb15198d45b1f8fa8bf0e36a12a26e5bb30f
FattiMei <teomei68@gmail.com>
2024-08-10 17:28:41 +0200

    Make overlapping plot lines visible

--- plot/convergence_history.py
+++ plot/convergence_history.py
@@ -3,6 +3,9 @@ import csvparser
 import matplotlib.pyplot as plt
 
 
+STYLE = ['-.', '-']
+
+
 if __name__ == '__main__':
     if len(sys.argv) != 2:
         print(f"Usage: python {__file__} <csv_file_path>")
@@ -15,7 +18,7 @@ if __name__ == '__main__':
     plt.ylabel("$||r||$")
 
     for i in range(1,len(columns)):
-        plt.semilogy(df[columns[0]], df[columns[i]], label=columns[i])
+        plt.semilogy(df[columns[0]], df[columns[i]], STYLE[i % len(STYLE)], label=columns[i])
 
     plt.legend()
     plt.show()
