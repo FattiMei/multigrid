2bfbc09dae8f7e2188f9fccec5d08c64acba9c09
FattiMei <teomei68@gmail.com>
2024-08-12 06:52:55 +0200

    Add check for multigrid cycle recipe

--- include/multigrid.hpp
+++ include/multigrid.hpp
@@ -16,21 +16,23 @@ enum class MgOp {
 
 class MgSolver : public IterativeSolver {
 	public:
-		MgSolver(const Poisson1D &problem);
+		MgSolver(const Poisson1D &problem, const std::vector<MgOp> recipe, InitializationStrategy strategy);
 
 		void step();
 
 
 	protected:
+		const std::vector<MgOp> recipe;
 
 };
 
 
 void injective_restriction  (const int n, const double src[], double dest[]);
 void full_weight_restriction(const int n, const double src[], double dest[]);
+void linear_prolongation    (const int m, const double src[], double dest[]);
 
 
-void linear_prolongation(const int m, const double src[], double dest[]);
+bool analyze_cycle_recipe(const std::vector<MgOp> &recipe, int &nlevels);
 
 
 #endif
--- src/multigrid.cpp
+++ src/multigrid.cpp
@@ -1,4 +1,15 @@
 #include "multigrid.hpp"
+#include <iostream>
+
+
+MgSolver::MgSolver(const Poisson1D &problem, const std::vector<MgOp> recipe_, InitializationStrategy strategy) : IterativeSolver(problem, strategy), recipe(recipe_) {
+	// very hard memory allocations to understand
+}
+
+
+void MgSolver::step() {
+
+}
 
 
 // all these implementations assume that pointers have sufficient size
@@ -30,4 +41,44 @@ void linear_prolongation(const int m, const double src[], double dest[]) {
 	}
 
 	dest[write_index] = src[m-1];
+}
+
+
+bool analyze_cycle_recipe(const std::vector<MgOp> &recipe, int &nlevels) {
+	int curr_level = 0;
+	int max_depth  = 0;
+
+	for (const auto op : recipe) {
+		switch (op) {
+			case MgOp::Restrict:
+				++curr_level;
+				max_depth = std::max(max_depth, curr_level);
+				break;
+
+			case MgOp::Prolong:
+				--curr_level;
+
+				if (curr_level < 0) {
+					std::cerr << "[ERROR]: multigrid recipe wants to go above finest grid" << std::endl;
+					return false;
+				}
+
+				break;
+
+			case MgOp::Relax:
+			case MgOp::DirectSolve:
+			case MgOp::IterativeSolve:
+				break;
+		}
+	}
+
+	nlevels = max_depth;
+
+	if (curr_level == 0) {
+		return true;
+	}
+	else {
+		std::cerr << "[ERROR]: multigrid cycle doesn't end on the main grid" << std::endl;
+		return false;
+	}
 }
