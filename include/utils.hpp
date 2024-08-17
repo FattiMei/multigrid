b1f986499cf41f5d6423d0c46219354cf944400c
FattiMei <teomei68@gmail.com>
2024-08-17 17:03:13 +0200

    Add random vector generation for testing purpuoses

--- include/utils.hpp
+++ include/utils.hpp
@@ -2,7 +2,11 @@
 #define __UTILS_HPP__
 
 
+#include <vector>
+
+
 double linspace(double inf, double sup, int n, int i);
+std::vector<double> generate_random_vector(int n, double inf, double sup, int seed = 0);
 
 
 #endif
--- src/utils.cpp
+++ src/utils.cpp
@@ -1,6 +1,20 @@
 #include "utils.hpp"
+#include <random>
 
 
 double linspace(double inf, double sup, int n, int i) {
 	return inf + static_cast<double>(i) * (sup - inf) / static_cast<double>(n - 1);
+}
+
+
+std::vector<double> generate_random_vector(int n, double inf, double sup, int seed) {
+	std::default_random_engine gen(seed);
+	std::uniform_real_distribution<double> uniform(inf, sup);
+	std::vector<double> result(n);
+
+	for (auto &x : result) {
+		x = uniform(gen);
+	}
+
+	return result;
 }
