41472e4c98c0963c4a8cbb9f7a906a1028f85cba
FattiMei <teomei68@gmail.com>
2024-08-30 10:31:53 +0200

    Add symbolic exact function generation

---
+++ analysis/poisson_exact_solution.py
@@ -0,0 +1,41 @@
+import sys
+import sympy as sym
+from sympy.utilities.codegen import codegen
+
+
+# know issue: if the symbolic expression does not contain x or y,
+# the generated c function won't have it as an argument
+# more research could be done on this issue, for now pay attention to the generated code
+
+if __name__ == '__main__':
+    if len(sys.argv) != 3:
+        print(f'Usage: python {__file__} <src output path> <include output path>')
+        sys.exit(1)
+
+
+    x, y = sym.symbols('x y')
+
+    solution_1d     = sym.exp(-x*x*sym.sin(x / 10))
+    forcing_term_1d = -sym.diff(solution_1d,x,2)
+
+    # TODO: fix the issue described above
+    solution_2d     = sym.exp(x)*sym.exp(-2*y)
+    forcing_term_2d = -sym.diff(solution_2d,x,2) - sym.diff(solution_2d,y,2)
+
+    [(c_name, c_code), (h_name, c_header)] = codegen(
+        [
+            ('solution_1d', solution_1d),
+            ('forcing_term_1d', forcing_term_1d),
+            ('solution_2d', solution_2d),
+            ('forcing_term_2d', forcing_term_2d)
+        ],
+        'C99',
+        header=True,
+        empty=True
+    )
+
+    with open(sys.argv[1], 'w') as out:
+        out.write(c_code)
+
+    with open(sys.argv[2], 'w') as out:
+        out.write(c_header)
---
+++ artifacts/symbolic.cpp
@@ -0,0 +1,41 @@
+/******************************************************************************
+ *                      Code generated with SymPy 1.12.1                      *
+ *                                                                            *
+ *              See http://www.sympy.org/ for more information.               *
+ *                                                                            *
+ *                       This file is part of 'project'                       *
+ ******************************************************************************/
+#include "solution_1d.h"
+#include <math.h>
+
+double solution_1d(double x) {
+
+   double solution_1d_result;
+   solution_1d_result = exp(-pow(x, 2)*sin((1.0/10.0)*x));
+   return solution_1d_result;
+
+}
+
+double forcing_term_1d(double x) {
+
+   double forcing_term_1d_result;
+   forcing_term_1d_result = -((1.0/100.0)*pow(x, 2)*pow(x*cos((1.0/10.0)*x) + 20*sin((1.0/10.0)*x), 2) + (1.0/100.0)*pow(x, 2)*sin((1.0/10.0)*x) - 2.0/5.0*x*cos((1.0/10.0)*x) - 2*sin((1.0/10.0)*x))*exp(-pow(x, 2)*sin((1.0/10.0)*x));
+   return forcing_term_1d_result;
+
+}
+
+double solution_2d(double x, double y) {
+
+   double solution_2d_result;
+   solution_2d_result = exp(x)*exp(-2*y);
+   return solution_2d_result;
+
+}
+
+double forcing_term_2d(double x, double y) {
+
+   double forcing_term_2d_result;
+   forcing_term_2d_result = -5*exp(x)*exp(-2*y);
+   return forcing_term_2d_result;
+
+}
