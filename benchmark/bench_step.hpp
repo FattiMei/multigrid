c4167a8a7f42cacc24ab5dc36a2b6f387de0ad4f
FattiMei <teomei68@gmail.com>
2024-09-02 22:45:19 +0200

    refactor: benchmark uses a single templated function for controlling domain parameters

---
+++ benchmark/bench_step.hpp
@@ -0,0 +1,36 @@
+#ifndef __BENCH_STEP_HPP
+#define __BENCH_STEP_HPP
+
+
+#include "symbolic.h"
+#include "poisson.hpp"
+
+
+template <int DEPTH, auto CYCLE, auto SMOOTHER>
+static void BM_step(benchmark::State &state) {
+	const int n = state.range(0) + 1;
+
+	IsotropicPoisson2D problem(
+		{0.0, 0.0},
+		{1.0, 1.0},
+		n,
+		forcing_term_2d,
+		solution_2d
+	);
+
+	MgSolver solver(
+		&problem,
+		CYCLE(DEPTH, 3, true),
+		InitializationStrategy::Zeros,
+		SMOOTHER,
+		full_weight_restriction_2d,
+		linear_prolongation_2d
+	);
+
+	for (auto _ : state) {
+		solver.step();
+	}
+}
+
+
+#endif
--- benchmark/bench_vcycle.cpp
+++ benchmark/bench_vcycle.cpp
@@ -1,36 +1,9 @@
 #include <benchmark/benchmark.h>
-#include "symbolic.h"
-#include "poisson.hpp"
 #include "multigrid.hpp"
+#include "bench_step.hpp"
 
 
-template <int DEPTH, UpdateStrategy SMOOTHER>
-static void vcycle_step(benchmark::State &state) {
-	const int n = state.range(0) + 1;
-
-	IsotropicPoisson2D problem(
-		{0.0, 0.0},
-		{1.0, 1.0},
-		n,
-		forcing_term_2d,
-		solution_2d
-	);
-
-	MgSolver solver(
-		&problem,
-		MgCycle::V(DEPTH, 3),
-		InitializationStrategy::Zeros,
-		SMOOTHER,
-		full_weight_restriction_2d,
-		linear_prolongation_2d
-	);
-
-	for (auto _ : state) {
-		solver.step();
-	}
-}
-
-
-BENCHMARK(vcycle_step<2, UpdateStrategy::GaussSeidel>)->RangeMultiplier(2)->Range(4, 256);
-BENCHMARK(vcycle_step<2, UpdateStrategy::RedBlack>   )->RangeMultiplier(2)->Range(4, 256);
+BENCHMARK(BM_step<2,MgCycle::V,UpdateStrategy::GaussSeidel>)->RangeMultiplier(2)->Range(4, 256);
+BENCHMARK(BM_step<2,MgCycle::V,UpdateStrategy::RedBlack>   )->RangeMultiplier(2)->Range(4, 256);
+BENCHMARK(BM_step<2,MgCycle::V,UpdateStrategy::SOR>        )->RangeMultiplier(2)->Range(4, 256);
 BENCHMARK_MAIN();
