7354aa456a2891c58639bea2eb5520c88f50b9b8
FattiMei <teomei68@gmail.com>
2024-09-03 19:49:41 +0200

    Improve test on 2d grid transfer operators

--- test/test_restriction.cpp
+++ test/test_restriction.cpp
@@ -20,21 +20,16 @@ void print_2d(const std::vector<double>& X) {
 
 
 int main() {
-	std::vector<double> fine {
-		0.0, 0.0, 0.0, 0.0, 0.0,
-		0.0, 1.0, 2.0, 3.0, 0.0,
-		0.0, 4.0, 5.0, 6.0, 0.0,
-		0.0, 7.0, 8.0, 9.0, 0.0,
-		0.0, 0.0, 0.0, 0.0, 0.0
-	};
+	std::vector<double> fine(9*9);
+	for (size_t i = 0; i < fine.size(); ++i) fine[i] = i;
 
-	std::vector<double> coarse(3*3);
-	full_weight_restriction_2d({5,5}, fine.data(), coarse.data());
+	std::vector<double> coarse(5*5);
+	full_weight_restriction_2d({9,9}, fine.data(), coarse.data());
 
 	print_2d(fine);
 	print_2d(coarse);
 
-	linear_prolongation_2d({5,5}, coarse.data(), fine.data());
+	linear_prolongation_2d({9,9}, coarse.data(), fine.data());
 	print_2d(fine);
 
 	return 0;
