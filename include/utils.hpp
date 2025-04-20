9962100b28e8fb57afdbeb847d2abd776049eb8b
FattiMei <teomei68@gmail.com>
2025-04-20 18:34:18 +0200

    refactor: make linspace function return a vector

--- include/utils.hpp
+++ include/utils.hpp
@@ -5,11 +5,7 @@
 #include <vector>
 
 
-double linspace(double inf, double sup, int n, int i);
-std::vector<double> generate_random_vector(int n, double inf, double sup, int seed = 0);
-
-double linfnorm(const std::vector<double> &x, const std::vector<double> &y);
-double l2norm  (const std::vector<double> &x, const std::vector<double> &y);
+std::vector<double> linspace(double inf, double sup, int n);
 
 
 #endif
--- src/utils.cpp
+++ src/utils.cpp
@@ -1,45 +1,14 @@
 #include "utils.hpp"
-#include <cmath>
-#include <random>
+#include <cassert>
 
 
-double linspace(double inf, double sup, int n, int i) {
-	return inf + static_cast<double>(i) * (sup - inf) / static_cast<double>(n - 1);
-}
+std::vector<double> linspace(double inf, double sup, int n) {
+	assert(n > 0);
+	std::vector<double> mesh(n);
 
-
-std::vector<double> generate_random_vector(int n, double inf, double sup, int seed) {
-	std::default_random_engine gen(seed);
-	std::uniform_real_distribution<double> uniform(inf, sup);
-	std::vector<double> result(n);
-
-	for (auto &x : result) {
-		x = uniform(gen);
+	for (int i = 0; i < n; ++i) {
+		mesh[i] = inf + static_cast<double>(i) * (sup - inf) / static_cast<double>(n - 1);
 	}
 
-	return result;
-}
-
-
-double linfnorm(const std::vector<double> &x, const std::vector<double> &y) {
-	double max = 0.0;
-
-	for (size_t i = 0; i < x.size(); ++i) {
-		max = std::max(max, std::abs(x[i] - y[i]));
-	}
-
-	return max;
-}
-
-
-double l2norm(const std::vector<double> &x, const std::vector<double> &y) {
-	double acc = 0.0;
-
-	for (size_t i = 0; i < x.size(); ++i) {
-		const double diff = x[i] - y[i];
-
-		acc += diff * diff;
-	}
-
-	return std::sqrt(acc);
+	return mesh;
 }
